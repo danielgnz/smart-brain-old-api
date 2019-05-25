c88ab2fffc301ff09067b98ed945a617abf11d9f
Daniel Buhaianu <danibtechnology@gmail.com>
2019-05-25 21:57:22 +0100

    edited / request

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(db.users) });
+app.get('/', (req, res) => { res.send(`it is working!`) });
 app.get('/profile/:id', profile.handleProfile(db));
 
 app.post('/signin', signin.handleSignIn(db, bcrypt));
