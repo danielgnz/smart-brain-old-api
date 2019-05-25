eb3ae25f9e0009884704e6835142a5c521a6bedd
Daniel Buhaianu <danibtechnology@gmail.com>
2019-05-25 21:16:55 +0100

    code refactoring and security checks

---
+++ controllers/register.js
@@ -0,0 +1,40 @@
+const saltRounds = 10;
+
+const handleRegister = (db, bcrypt) => (req, res) => {
+	const { username, email, password } = req.body;
+	
+	if(!email || !password || !username){
+		return res.status(400).json('incorrect form submission');
+	}
+
+	const salt = bcrypt.genSaltSync(saltRounds);
+	const hash = bcrypt.hashSync(password, salt);
+	
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				username: username,
+				email: loginEmail[0],
+				joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('unable to register'))
+};
+
+module.exports = {
+	handleRegister
+}
--- server.js
+++ server.js
@@ -1,102 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image =  require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'admin',
+		database: 'smart-brain'
+	}
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '12347',
-			username: 'danielbuhaianu',
-			email: 'test@gmail.com',
-			password: 'test',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '9836',
-			username: 'marianfinaru',
-			email: 'MarianFinaru@gmail.com',
-			password: 'bananas2',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '3589',
-			username: 'bejaneduard',
-			email: 'bejanEduard@gmail.com',
-			password: 'mandarine-portocale',
-			entries: 0,
-			joined: new Date()
-		},
-	]
-};
+app.get('/', (req, res) => { res.send(db.users) });
+app.get('/profile/:id', profile.handleProfile(db));
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-})
+app.post('/signin', signin.handleSignIn(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
 
-app.get('/profile/:id', (req, res) => {
-	const  { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			return res.json(user);
-		}
-	});
-	if(!found){
-		res.status(404).json('no such user');
-	}
-})
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleApiCall());
 
-app.post('/signin', (req, res) => {
-	const { email, password } = req.body;
-	const filteredDatabase = database.users.filter(user => {
-		return user.email.toLowerCase() === email.toLowerCase() &&
-			   user.password === password
-	});
-	if(filteredDatabase.length > 0){
-		res.json(filteredDatabase[0]);
-	}
-	else {
-		res.status(400).json('error logging in');
-	}
-})
-
-app.post('/register', (req, res) => {
-	const { username, email, password } = req.body;
-	database.users.push({
-		id: '122323',
-		username: username,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date()
-	});
-	res.json(database.users[database.users.length - 1]);
-})
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			user.entries++;
-			res.json(user.entries);
-		}
-	});
-	if(!found){
-		res.status(404).json('error. user not found');
-	}
-})
-
-app.listen(8080, () => {
-	console.log('app is running on port 8080');
-})
+app.listen(8080, () => { console.log('app is running on port 8080'); });
