9f4fa9cfa0738ae4406f7963b245e58ff3c5549e
Daniel Buhaianu <danibtechnology@gmail.com>
2019-05-26 09:08:59 +0100

    added pool connection

--- server.js
+++ server.js
@@ -17,12 +17,32 @@ const db = knex({
 	}
 });
 
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionSring: process.env.DATABASE_URL,
+	ssl: true
+});
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { res.send(`it is working!`) });
+app.get('/db', async (req,res) => {
+	try {
+		const client = await pool.connect()
+		const result = await client.query('SELECT * FROM users');
+		const results ={ 'results': (result) ? result.rows : null };
+		res.render('pages/db', results);
+		client.release();
+	}
+	catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+});
+
 app.get('/profile/:id', profile.handleProfile(db));
 
 app.post('/signin', signin.handleSignIn(db, bcrypt));
