3252e518b605ffe03f92f3aff63d92dc92b7ceb1
Daniel Buhaianu <danibtechnology@gmail.com>
2019-05-26 09:42:03 +0100

    new changes

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const db = knex({
 	}
 });
 
-const { Pool } = require('pg');
-const pool = new Pool({
-	connectionSring: process.env.DATABASE_URL,
-	ssl: true
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+// 	connectionSring: process.env.DATABASE_URL,
+// 	ssl: true
+// });
 
 const app = express();
 
@@ -29,19 +29,19 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { res.send(`it is working!`) });
-app.get('/db', async (req,res) => {
-	try {
-		const client = await pool.connect()
-		const result = await client.query('SELECT * FROM test_table');
-		const results ={ 'results': (result) ? result.rows : null };
-		res.render('pages/db', results);
-		client.release();
-	}
-	catch (err) {
-		console.error(err);
-		res.send("Error " + err);
-	}
-});
+// app.get('/db', async (req,res) => {
+// 	try {
+// 		const client = await pool.connect()
+// 		const result = await client.query('SELECT * FROM test_table');
+// 		const results ={ 'results': (result) ? result.rows : null };
+// 		res.render('pages/db', results);
+// 		client.release();
+// 	}
+// 	catch (err) {
+// 		console.error(err);
+// 		res.send("Error " + err);
+// 	}
+// });
 
 app.get('/profile/:id', profile.handleProfile(db));
 
